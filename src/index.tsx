6f542bcb5d4bcacc551e8e59b06396a9f21be137
sumeet.rana <sumeet.rana@innovaccer.com>
2022-02-06 20:10:13 +0530

    esbuild.wasm startService added

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
     const [input, setInput] = useState("");
     const [code, setCode] = useState("");
 
+    const startService = async () => {
+        const service = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+        console.log(service);
+        
+    }
+
+    useEffect(() => {
+        startService();
+    }, []);
+
     const onClick = () => {
         console.log(input);
-        
     }
 
     return (
