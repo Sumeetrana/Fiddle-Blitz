9b9d7156ad451b06de832e5e68b124efdfd07f20
sumeet.rana <sumeet.rana@innovaccer.com>
2022-02-06 20:37:49 +0530

    TRANSPILING WORKS(ESBUILD)git add .!

--- src/index.tsx
+++ src/index.tsx
@@ -18,10 +18,15 @@ const App = () => {
         startService();
     }, []);
 
-    const onClick = () => {
+    const onClick = async () => {
         if(!ref.current) return;
 
-        console.log(ref.current);
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        })
+
+        setCode(result.code);
         
     }
 
@@ -31,7 +36,7 @@ const App = () => {
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre></pre>
+            <pre>{code}</pre>
         </div>
     )
 }
