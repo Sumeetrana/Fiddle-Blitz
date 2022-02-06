e175c1d5b2f77145c79a7f66996c90757c24b9cb
sumeet.rana <sumeet.rana@innovaccer.com>
2022-02-06 19:56:38 +0530

    building the basic form structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <h1>Hi!</h1>
+    const [input, setInput] = useState("");
+    const [code, setCode] = useState("");
+
+    const onClick = () => {
+        console.log(input);
+        
+    }
+
+    return (
+        <div>
+            <textarea onChange={e => setInput(e.target.value)}></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre></pre>
+        </div>
+    )
 }
 
 ReactDOM.render(
